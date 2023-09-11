67a10b79930ca89fa01516b5ef08f1f5b1faeaaa
Rup medhi <rupmmedhi@gmail.com>
2023-09-11 19:27:46 +0530

    PUT API update data

---
+++ src/app/api/products/[product_id]/route.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+import { connectionString } from "@/lib/db";
+import product from "@/lib/model/product";
+
+export async function PUT(request, content) {
+
+    const productId = content.params.product_id
+    const filter = { _id: productId }
+    const payload = await request.json()
+    await mongoose.connect(connectionString)
+    const result = await product.findOneAndUpdate(filter, payload)
+
+
+    return NextResponse.json({ result, sucess: true })
+
+}
